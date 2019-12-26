321c316868b84d6cfaca6cdb9ed8f53182f20943
James Cho <panamajim@gmail.com>
2019-12-25 23:39:01 -0800

    added cart endpoint

---
+++ server/public/api/cart.php
@@ -0,0 +1,17 @@
+<?php
+
+define('INTERNAL', true);
+require_once('functions.php');
+session_start();
+set_exception_handler('error_handler');
+require_once('db_connection.php');
+switch($_SERVER['REQUEST_METHOD']){
+  case 'GET':
+    require_once('cart_get.php');
+    break;
+  case 'POST':
+    require_once('cart_add.php');
+    break;
+}
+
+?>
