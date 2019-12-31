67325235eac4431b6ad9f9cbf5e89ba9a9b59c8e
James Cho <panamajim@gmail.com>
2019-12-31 20:35:42 +0000

    removed view state change in header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  render() {
-    return (
-      <div className='headerContainer d-flex mb-3'>
-        <div className='container d-flex py-3 px-5 w-100 align-items-center justify-content-between'>
-          <Link to={'/products'}>
-            <h1 className='header-name'>Lego Collector</h1>
-          </Link>
-          <Link to='/cart' >
-            <h5 className='shopping-cart-container d-flex align-items-center' >
-              <img className="shoppingCartImage mr-2" src='/images/shopping.png' />
-              {`  ${this.props.numberOfItemsInCart}`}
-            </h5>
-          </Link>
-        </div>
+export default function Header(props) {
+  return (
+    <div className='headerContainer d-flex mb-3'>
+      <div className='container d-flex py-3 px-5 w-100 align-items-center justify-content-between'>
+        <Link to={'/products'}>
+          <h1 className='header-name'>Lego Collector</h1>
+        </Link>
+        <Link to='/cart' >
+          <h5 className='shopping-cart-container d-flex align-items-center' >
+            <img className="shoppingCartImage mr-2" src='/images/shopping.png' />
+            {`  ${props.numberOfItemsInCart}`}
+          </h5>
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
 }
