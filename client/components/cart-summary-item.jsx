e3c2ed2dc2bc5919a5121d25dc135a27d3ea3988
James Cho <panamajim@gmail.com>
2019-12-31 21:26:57 +0000

    fixed cart items styling and spacing

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 
 export default function CartSummaryItem(props) {
   return (
-    <div>
-      <div className="container d-flex justify-content-center border rounded my-2 cart-summary-item-container">
-        <div className="image-container d-flex col-5 p-2">
-          <img src={props.itemImage} className="cart-item-img rounded m-auto" />
-        </div>
-        <div className="d-flex-column p-2 col-5">
-          <h5 className="item-name">{props.itemName}</h5>
-          <div className="item-price mb-2">{'$' + (props.itemPrice / 100)}</div>
-          <div className="item-description mb-2">{props.itemDescription}</div>
-        </div>
-        <div className="d-flex flex-column col-2 justify-content-center align-items-center item-quantity-container">
-          <h5>Qty</h5>
-          <h5>{props.itemQuantity}</h5>
-        </div>
+    <div className="container d-flex justify-content-center border rounded my-2 cart-item-container">
+      <div className="image-container d-flex col-5 p-2">
+        <img src={props.itemImage} className="cart-item-img rounded m-auto" />
+      </div>
+      <div className="d-flex-column p-2 col-5">
+        <h5 className="item-name">{props.itemName}</h5>
+        <div className="item-price mb-2">{'$' + (props.itemPrice / 100)}</div>
+        <div className="item-description mb-2">{props.itemDescription}</div>
+      </div>
+      <div className="d-flex flex-column col-2 justify-content-center align-items-center item-quantity-container">
+        <h5>Qty</h5>
+        <h5>{props.itemQuantity}</h5>
       </div>
     </div>
   );
