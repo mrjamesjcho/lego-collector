3b0d424942b4a1f409cd5a31b347b783360ce0c8
James Cho <panamajim@gmail.com>
2019-12-31 23:03:33 +0000

    cart displays name and no longer description

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -7,9 +7,8 @@ export default function CartSummaryItem(props) {
         <img src={props.itemImage} className="cart-item-img rounded m-auto" />
       </div>
       <div className="d-flex-column p-2 col-5">
-        <h5 className="item-name">{props.itemName}</h5>
+        <h4 className="item-name">{props.itemName}</h4>
         <div className="item-price mb-2">{'$' + (props.itemPrice / 100)}</div>
-        <div className="item-description mb-2">{props.itemDescription}</div>
       </div>
       <div className="d-flex flex-column col-2 justify-content-center align-items-center item-quantity-container">
         <h5>Qty</h5>
