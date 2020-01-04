f68a9dcf6c48fe82ec98cbbe86054ca3a81797fb
James Cho <panamajim@gmail.com>
2020-01-04 06:33:36 +0000

    fixed styling for cart and changed class names to camelcase

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -34,17 +34,17 @@ export default class CartSummary extends React.Component {
       });
     }
     return (
-      <div className="cart-summary-container container col-lg-8 align-self-center mb-5">
-        <div className="row px-4 pb-2 back-to-catalog-container">
-          <Link to='/products' className='back-to-catalog'>&lt; <u>continue shopping</u></Link>
+      <div className="cartContainer container col-lg-8 align-self-center mb-5">
+        <div className="backToCatalogContainer pb-2">
+          <Link to='/products' className='backToCatalog'>&lt; <u>continue shopping</u></Link>
         </div>
-        <h1 className="cart-summary-header container">Shopping Cart</h1>
-        <div className="cart-summary-items container d-flex-column">
+        <h1 className="cartHeader">Shopping Cart</h1>
+        <div className="cartItems d-flex-column">
           {cartItemElements}
         </div>
-        <div className="cart-total-checkout-container container d-flex">
-          <h1 className="cart-total my-auto">Item Total: {'$' + (itemTotal / 100)}</h1>
-          <button className="cart-checkout btn btn-primary ml-auto">Checkout</button>
+        <div className="cartTotalCheckoutContainer d-flex">
+          <h1 className="cartTotal my-auto">Item Total: {'$' + (itemTotal / 100)}</h1>
+          <button className="checkoutBtn btn btn-primary ml-auto">Checkout</button>
         </div>
       </div>
     );
