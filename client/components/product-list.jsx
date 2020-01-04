beb8e212bd8410ea93303f5bbb53b901e7bacb02
James Cho <panamajim@gmail.com>
2020-01-04 04:31:22 +0000

    changed product-list into a stateless component

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -3,32 +3,24 @@
 import React from 'react';
 import ProductListItem from './product-list-item';
 
-export default class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: []
-    };
-  }
-  render() {
-    const elements = this.props.products.map(product => {
-      return (
-        <ProductListItem
-          key={product.id}
-          product={product}
-          id={product.id}
-          item={product}
-          itemImages={product.images}
-          itemName={product.name}
-          itemPrice={product.price}
-          itemDescription={product.shortDescription}
-          onAddCartItem={this.props.onAddCartItem} />
-      );
-    });
+export default function ProductList(props) {
+  const elements = props.products.map(product => {
     return (
-      <div className="container d-flex flex-wrap mb-5">
-        {elements}
-      </div>
+      <ProductListItem
+        key={product.id}
+        product={product}
+        id={product.id}
+        item={product}
+        itemImages={product.images}
+        itemName={product.name}
+        itemPrice={product.price}
+        itemDescription={product.shortDescription}
+        onAddCartItem={props.onAddCartItem} />
     );
-  }
+  });
+  return (
+    <div className="container d-flex flex-wrap mb-5">
+      {elements}
+    </div>
+  );
 }
