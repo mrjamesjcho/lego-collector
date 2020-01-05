3787edc2474baa589442a49877f6879aa19054e6
James Cho <panamajim@gmail.com>
2020-01-05 06:38:17 +0000

    added color to carousel arrows to better see them

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -33,11 +33,11 @@ export default function Carousel(props) {
           {carouselElements}
         </div>
         <a className="carousel-control-prev" href="#carouselCaptions" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="carouselPrev carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="sr-only">Previous</span>
         </a>
         <a className="carousel-control-next" href="#carouselCaptions" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="carouselNext carousel-control-next-icon" aria-hidden="true"></span>
           <span className="sr-only">Next</span>
         </a>
       </div>
