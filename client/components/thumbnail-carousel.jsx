0ce36fea9b4881f730e2ffa494a2ff49b7449a14
James Cho <panamajim@gmail.com>
2020-01-05 19:34:19 +0000

    initial build of thumbnail-carousel component

---
+++ client/components/thumbnail-carousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ThumbnailCarousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      thumbnails: props.thumbnails
+    };
+  }
+  render() {
+    return <div></div>;
+  }
+}
