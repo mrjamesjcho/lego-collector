b298cb24c6227fc1bafc50e3f94fd5ac3d820a9b
James Cho <panamajim@gmail.com>
2020-01-05 04:30:16 +0000

    products passed into carousel and carousel items dynamically rendered

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -1,45 +1,42 @@
 import React from 'react';
 
 export default function Carousel(props) {
-  // const indicatorElements = [];
-  // const carouselElements = [];
-
+  const indicatorElements = [];
+  const carouselElements = [];
+  props.featured.map((product, index) => {
+    indicatorElements.push(
+      <li
+        key={index}
+        className={index === 0 ? 'active' : ''}
+        data-target="#carouselCaptions"
+        data-slide-to={index} ></li>
+    );
+    carouselElements.push(
+      <div
+        key={index}
+        className={`carouselItem carousel-item ${index === 0 ? 'active' : ''}`} >
+        <img src={product.images[0]} className="d-block w-100" alt={product.name} />
+        <div className="carousel-caption d-none d-md-block">
+          <h3>{product.name}</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+      </div>
+    );
+  });
   return (
     <div className="carouselContainer container my-2">
       <div id="carouselCaptions" className="carousel slide" data-ride="carousel">
         <ol className="carousel-indicators">
-          <li data-target="#carouselCaptions" data-slide-to="0" className="active"></li>
-          <li data-target="#carouselCaptions" data-slide-to="1"></li>
-          <li data-target="#carouselCaptions" data-slide-to="2"></li>
+          {indicatorElements}
         </ol>
         <div className="carousel-inner">
-          <div className="carouselItem carousel-item active">
-            <img src="/images/1989-batmobile-1.jpeg" className="d-block w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h3>1989 Batmobie</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-          </div>
-          <div className="carouselItem carousel-item">
-            <img src="/images/bugatti-1.jpeg" className="d-block w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h3>Bugatti Chiron</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
-          <div className="carouselItem carousel-item">
-            <img src="/images/millennium-falcon-1.jpeg" className="d-block w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h3>Millenium Falcon</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </div>
-          </div>
+          {carouselElements}
         </div>
-        <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <a className="carousel-control-prev" href="#carouselCaptions" role="button" data-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="sr-only">Previous</span>
         </a>
-        <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <a className="carousel-control-next" href="#carouselCaptions" role="button" data-slide="next">
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="sr-only">Next</span>
         </a>
