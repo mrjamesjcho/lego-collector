9d2517b423b5e95a6b5803edcbe476a7144c15a5
James Cho <panamajim@gmail.com>
2020-01-12 18:28:56 +0000

    delete modal renders conditionally in cart-summary component instead of delete-confirm component

--- client/components/delete-confirm.jsx
+++ client/components/delete-confirm.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import './styles/delete-confirm.css';
 
 export default function DeleteConfirm(props) {
-  if (!props.item) {
-    return;
-  }
   return (
     <div className="deleteConfirmBackground d-flex justify-content-center align-items-center position-absolute h-100 w-100">
-      <div className="deleteConfirmContainer container col-4 bg-white p-3 border rounded">
+      <div className="deleteConfirmContainer col-4 bg-white p-3 border rounded">
         <h5 className="deleteConfirmItemName text-center">{props.item.name}</h5>
         <div className="deleteConfirmImgContainer d-flex p-2">
           <img
