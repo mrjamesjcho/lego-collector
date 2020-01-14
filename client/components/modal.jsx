5f43f8f9ca95f99eb5bac1ab440bf2d60c08d23f
James Cho <panamajim@gmail.com>
2020-01-14 07:53:30 +0000

    added x button to close modal

---
+++ client/components/modal.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Modal(props) {
+  return (
+    <div className="deleteConfirmBackground d-flex justify-content-center align-items-center fixed-top h-100 w-100">
+
+    </div>
+  );
+}
