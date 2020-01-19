afea1f40b12d8e8d823b4ac979a1ff59f460d05f
James Cho <panamajim@gmail.com>
2020-01-18 21:58:26 -0800

    order-confirm renders order items like cart

--- client/components/order-confirm.jsx
+++ client/components/order-confirm.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import CartSummaryItem from './cart-summary-item';
 
 export default function OrderConfirm(props) {
+  function renderOrderItems() {
+    var elements = null;
+    elements = props.OrderItems.map(item => {
+      return (
+        <CartSummaryItem
+          key={item.id}
+          item={item}
+          onUpdateCartItem={this.props.onUpdateCartItem}
+          onDeleteCartItem={this.handleDeleteCartItem} />
+      );
+    });
+    return elements;
+  }
+
   return (
-    <div></div>
+    <div className="orderContainer container mb-5">
+      <h1>Thank you for your order!</h1>
+      <h5 className="orderHeader ml-3">Your Order:</h5>
+      <div className="orderItems d-flex-column border-top">
+        {renderOrderItems()}
+      </div>
+    </div>
   );
 }
