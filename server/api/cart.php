5822bdf9f6fc904517febf7baa861b03af7920ec
James Cho <panamajim@gmail.com>
2020-01-20 16:55:12 +0000

    added patch method for cart endpoint

--- server/api/cart.php
+++ server/api/cart.php
@@ -30,13 +30,14 @@ if ($request['method'] === 'GET') {
   send($response);
 
 } else if ($request['method'] === 'POST') {
-  $link = get_db_link();
-  if(isset($request['body']['productId'])) {
 
+  if(isset($request['body']['productId'])) {
     $productId = $request['body']['productId'];
     if (!is_numeric($productId) || $productId <= 0) {
       throw new ApiError('valid productId is required', 400);
     }
+
+    $link = get_db_link();
 
     $priceQuery = "SELECT `price` FROM `products` WHERE `id` = $productId";
     $result = mysqli_query($link, $priceQuery);
@@ -65,7 +66,7 @@ if ($request['method'] === 'GET') {
     $insertResult = mysqli_query($link, $insertQuery);
     $cartItemId = mysqli_insert_id($link);
 
-    $responseQuery = "SELECT c.`cartItemId` AS `id`, c.`count`, c.`productId`, `products`.`name`, `products`.`price`,
+    $responseQuery = "SELECT c.`cartItemId` AS `id`, c.`productId`, `products`.`name`, `products`.`price`, c.`count`,
                       (SELECT i.url FROM product_images AS i WHERE c.`productId` = i.product_id LIMIT 1) AS image,
                       `products`.`shortDescription`
                       FROM cartItems AS c
@@ -77,13 +78,64 @@ if ($request['method'] === 'GET') {
     }
     $cartItemData = mysqli_fetch_assoc($result);
     $cartItemData['id'] = (int) $cartItemData['id'];
+    $cartItemData['productId'] = (int) $cartItemData['productId'];
     $cartItemData['count'] = (int) $cartItemData['count'];
     $cartItemData['price'] = (int) $cartItemData['price'];
 
     $response['body'] = $cartItemData;
     send($response);
-
   }
+
+  throw new ApiError('valid productId is required', 400);
+
+} else if ($request['method'] === 'PATCH') {
+  if (!isset($request['body']['id'])) {
+    throw new ApiError('cart item id required', 400);
+  }
+
+  $id = $request['body']['id'];
+  if (!is_numeric($id) || $id <= 0) {
+    throw new ApiError('invalid cart item id', 400);
+  }
+
+  $incDec = (int) $request['body']['incDec'];
+  if ($incDec !== 1 || $incDec !== -1) {
+    throw new ApiError('invalid inc dec value', 400);
+  }
+
+  $cartId = $_SESSION['cartId'];
+  $link = get_db_link();
+
+  $updateQuery = "UPDATE `cartItems` SET `count` = `count` + $incDec  WHERE `cartItems`.`productID` = $id AND `cartItems`.`cartID` = $cartId";
+  $updateResult = mysqli_query($link, $updateQuery);
+  if (!$updateResult) {
+    throw new ApiError(mysqli_error($link));
+  }
+  if (mysqli_affected_rows($link) < 1) {
+    throw new ApiError('error updating cart', 400);
+  }
+
+  $responseQuery = "SELECT c.`cartItemId` AS `id`, c.`productId`, `products`.`name`, `products`.`price`, c.`count`,
+                      (SELECT i.url FROM product_images AS i WHERE c.`productId` = i.product_id LIMIT 1) AS image,
+                      `products`.`shortDescription`
+                      FROM cartItems AS c
+                      JOIN products ON c.`productId` = `products`.`id`
+                      WHERE c.`cartItemId` = $id";
+  $result = mysqli_query($link, $responseQuery);
+  if (!$result) {
+    throw new ApiError(mysqli_error($link));
+  }
+  $cartItemData = mysqli_fetch_assoc($result);
+  $cartItemData['id'] = (int) $cartItemData['id'];
+  $cartItemData['productId'] = (int) $cartItemData['productId'];
+  $cartItemData['count'] = (int) $cartItemData['count'];
+  $cartItemData['price'] = (int) $cartItemData['price'];
+
+  $response['body'] = $cartItemData;
+  send($response);
+
+} else if ($request['method'] === 'DELETE') {
+
 }
 
 ?>
