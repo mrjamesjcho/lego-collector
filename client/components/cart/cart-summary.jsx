e6151a27d9c1753353c1af1e2c56a4944f506937
James Cho <panamajim@gmail.com>
2020-01-22 19:30:12 +0000

    added method to conditionally render checkout button whether there are items in cart

--- client/components/cart/cart-summary.jsx
+++ client/components/cart/cart-summary.jsx
@@ -36,6 +36,18 @@ export default class CartSummary extends React.Component {
     }
     return elements;
   }
+  renderCheckoutButton() {
+    if (this.props.cartItems.length) {
+      return (
+        <Link
+          to='/checkout'
+          className='ml-auto' >
+          <button className="checkoutBtn btn btn-warning">Proceed to checkout</button>
+        </Link>
+      );
+    }
+    return null;
+  }
   renderDeleteConfirmModal() {
     if (this.state.itemToRemove) {
       return (
@@ -62,11 +74,7 @@ export default class CartSummary extends React.Component {
         </div>
         <div className="cartTotalCheckoutContainer d-flex">
           <h4 className="cartTotal my-auto ml-3">Item Total: {'$' + (this.props.cartTotal / 100)}</h4>
-          <Link
-            to='/checkout'
-            className='ml-auto' >
-            <button className="checkoutBtn btn btn-warning">Proceed to checkout</button>
-          </Link>
+          {this.renderCheckoutButton()}
         </div>
         {this.renderDeleteConfirmModal()}
       </div>
