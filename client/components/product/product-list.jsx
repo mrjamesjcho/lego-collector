089794ca2d3ea7dc22172f48155f1e9db408c99b
James <mrjamesjcho@gmail.com>
2020-02-16 06:44:18 +0000

    started converting product components to material ui

--- client/components/product/product-list.jsx
+++ client/components/product/product-list.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+
+import { makeStyles, Container, Box } from '@material-ui/core';
+
 import Carousel from '../carousel/carousel';
 import ProductListItem from '../product/product-list-item';
 import '../styles/product-list.css';
 import '../styles/add-cart-confirm.css';
 
 export default function ProductList(props) {
+  const classes = useStyles();
+
   const elements = props.products.map(product => {
     return (
       <ProductListItem
@@ -16,11 +21,22 @@ export default function ProductList(props) {
   return (
     <React.Fragment>
       <Carousel featured={props.featured} />
-      <div className="productListContainer container p-sm-0 px-3">
-        <div className="productListBorder d-flex flex-wrap mb-5 border-top border-left">
+      <Container className={classes.productListContainer}>
+        <Box borderColor="grey.500" borderLeft={1} borderTop={1} className={classes.productListBorder}>
           {elements}
-        </div>
-      </div>
+        </Box>
+      </Container>
     </React.Fragment>
   );
 }
+
+const useStyles = makeStyles(({ spacing }) => ({
+  productListBorder: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginBottom: spacing(3)
+  },
+  productListContainer: {
+    padding: spacing(0, 3)
+  }
+}));
